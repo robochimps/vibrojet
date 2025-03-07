38577c43014b51a7e96fc969ed7179c8fdcc5ce8
Andrey <andrey.yachmenev@desy.de>
2025-03-07 22:25:41 +0100

    Add zmatrix coordinates to KEO and KEO for CH4 in symmetrized cos(alpha) coordinates

--- vibrojet/keo.py
+++ vibrojet/keo.py
@@ -1,5 +1,5 @@
 import functools
-from typing import Callable
+from typing import Callable, List, Tuple
 
 import jax
 import jax.numpy as jnp
@@ -188,3 +188,176 @@ def _Gmat_s(q, masses, internal_to_cartesian, cartesian_to_internal):
 
 
 batch_Gmat_s = jax.jit(jax.vmap(_Gmat_s, in_axes=(0, None, None, None)))
+
+
+def zmatrix_coordinates(
+    zmat_connections: List[Tuple[int, int, int, int]],
+    no_bonds: int,
+    no_angles: int,
+    internal: jnp.ndarray,
+    pm: int = 1,
+):
+    """Computes the Cartesian coordinates of atoms from given internal coordinates
+    in the Z-matrix format.
+
+    Args:
+        zmat_connections (List[Tuple[int, int, int, int]]): A list of tuples defining
+            the Z-matrix connectivity for each atom. Each tuple consists of:
+            - The index of the reference atom for a direct bond connection.
+            - The index of the second reference atom to define a bond angle.
+            - The index of the third reference atom for either a dihedral angle
+                or a secondary valence angle.
+            - An integer specifying the type of dihedral or secondary valence angle.
+        no_bonds (int): The number of bond distance internal coordinates.
+        no_angles (int): The number of angular internal coordinates.
+        internal (jnp.ndarray): A 1D array of shape (3N-6) containing the internal
+            coordinate values in the following order:
+            - Bond distances (first `no_bonds` elements).
+            - Valence angles (next `no_angles` elements).
+            - Dihedral angles or secondary valence angles (remaining elements).
+        pm (int, optional): A factor for controlling the sign of the z-coordinate
+            for the second atom. Defaults to 1.
+
+    Returns:
+        jnp.ndarray: A 2D array of shape (N, 3) containing the Cartesian coordinates
+        of N atoms.
+    """
+    assert (
+        abs(pm) == 1
+    ), f"Illegal value for sign parameter 'pm' = {pm} (can be 1 or -1)"
+
+    no_atoms = len(zmat_connections)
+    xyz = [jnp.array([0.0, 0.0, 0.0])]
+
+    n1 = jnp.array([0, 0, 1])
+    xyz.append(xyz[0] + n1 * internal[0])
+
+    if no_atoms > 2:
+        p1, *_ = zmat_connections[1]
+        p1 -= 1
+        r = internal[1]
+        alpha = internal[no_bonds]
+        n2 = jnp.array([jnp.sin(alpha), 0.0, pm * jnp.cos(alpha)])
+        xyz.append(xyz[p1 - 1] + n2 * r)
+
+    iangle = 0
+    idihedral = -1
+
+    for iatom in range(3, no_atoms):
+
+        r = internal[iatom]
+        iangle += iangle
+        alpha = internal[no_bonds + iangle]
+
+        p1, p2, p3, j = zmat_connections[iatom]
+        p1 -= 1
+        p2 -= 1
+        p3 -= 1
+
+        if j in (-1, 0):
+            iangle = iangle + 1
+            beta = internal[no_bonds + iangle]
+
+            v12 = xyz[p2] - xyz[p1]
+            v23 = xyz[p3] - xyz[p1]
+            n2 = v12 / jnp.linalg.norm(v12)
+            n3 = jnp.cross(v23, v12)
+            n3 = n3 / jnp.linalg.norm(n3)
+            n1 = jnp.cross(n2, n3)
+
+            cosa3 = jnp.sum(n2 * v23) / jnp.linalg.norm(v23)
+            alpha3 = jnp.acos(cosa3)
+            cosphi = (jnp.cos(beta) - jnp.cos(alpha) * jnp.cos(alpha3)) / (
+                jnp.sin(alpha) * jnp.sin(alpha3)
+            )
+            phi = jnp.acos(cosphi)
+
+            xyz.append(
+                xyz[p1]
+                + r
+                * (
+                    jnp.cos(alpha) * n2
+                    + jnp.sin(alpha) * jnp.cos(phi) * n1
+                    + jnp.sin(alpha) * jnp.sin(phi) * n3
+                )
+            )
+
+        elif j in (1,):
+            idihedral = idihedral + 1
+            phi = internal[no_bonds + no_angles + idihedral]
+
+            v12 = xyz[p2] - xyz[p1]
+            v23 = xyz[p3] - xyz[p1]
+            n2 = v12 / jnp.linalg.norm(v12)
+            n3 = jnp.cross(v12, v23)
+            n3 = n3 / jnp.linalg.norm(n3)
+            n1 = jnp.cross(n2, n3)
+
+            xyz.append(
+                xyz[p1]
+                + r
+                * (
+                    jnp.cos(alpha) * n2
+                    + jnp.sin(alpha) * jnp.cos(phi) * n1
+                    - jnp.sin(alpha) * jnp.sin(phi) * n3
+                )
+            )
+
+        elif j in (-2, 2):
+            idihedral = idihedral + 1
+            phi = internal[no_bonds + no_angles + idihedral]
+
+            v12 = xyz[p2] - xyz[p1]
+            v23 = xyz[p3] - xyz[p2]
+            n2 = v12 / jnp.linalg.norm(v12)
+            n3 = jnp.cross(v23, v12)
+            n3 = n3 / jnp.linalg.norm(n3)
+            n1 = jnp.cross(n2, n3)
+
+            if j < 0:
+                n3 = -n3
+
+            xyz.append(
+                xyz[p1]
+                + r
+                * (
+                    jnp.cos(alpha) * n2
+                    + jnp.sin(alpha) * jnp.cos(phi) * n1
+                    - jnp.sin(alpha) * jnp.sin(phi) * n3
+                )
+            )
+
+        elif j >= 4 and j <= 100:
+            iangle = iangle + 1
+            beta = internal[no_bonds + iangle]
+
+            v12 = xyz[p2] - xyz[p1]
+            v23 = xyz[p3] - xyz[p1]
+            n2 = v12 / jnp.linalg.norm(v12)
+            n3 = jnp.cross(v12, v23)
+            n3 = n3 / jnp.linalg.norm(n3)
+            n1 = jnp.cross(n3, n2)
+
+            cosa3 = jnp.sum(n2 * v23) / jnp.linalg.norm(v23)
+            alpha3 = jnp.acos(cosa3)
+            cosphi = (jnp.cos(beta) - jnp.cos(alpha) * jnp.cos(alpha3)) / (
+                jnp.sin(alpha) * jnp.sin(alpha3)
+            )
+            phi = jnp.acos(cosphi)
+
+            xyz.append(
+                xyz[p1]
+                + r
+                * (
+                    jnp.cos(alpha) * n2
+                    + jnp.sin(alpha) * jnp.cos(phi) * n1
+                    + jnp.sin(alpha) * jnp.sin(phi) * n3
+                )
+            )
+
+        else:
+            raise ValueError(
+                f"Type of dihedral angle in 'zmat_connections' = {j}"
+                + f"for atom number {iatom} is not implemented"
+            )
+    return jnp.array(xyz)
